c5ffd2e6df15bb0c44b3a5c431cd5b0e7bf8e485
Mujib517 <mujib517@gmail.com>
2017-12-18 08:57:48 +0530

    Components and Modules

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app',
+    template: '<h1>Hello Agnular</h1>'
 })
-export class AppComponent {
-  title = 'app';
-}
+export class AppComponent { }
