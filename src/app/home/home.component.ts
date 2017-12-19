8f25648a838a9cf298cbc48df4a871ee1dfd95c3
Mujib517 <mujib517@gmail.com>
2017-12-19 08:58:34 +0530

    Data binding

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,6 +3,43 @@ import { Component } from "@angular/core";
 //ES6 Module
 @Component({
     selector: 'app-home',
-    template: '<h1>Home Page</h1>'
+    template: `<h1 [class]="cls">{{getTitle()}}</h1>
+    
+    <h1 [style]="style">{{title?"Yes":"No"}}</h1>
+
+    <button [disabled]="getTitle()">Click me</button>
+
+    <button (click)="onClick(1)">Button 2</button>
+
+    <select (change)="onChanged()">
+        <option>Apple</option>
+        <option>Samsung</option>
+        <option>Sony</option>
+    </select>
+
+    `
 })
-export class HomeComponent { }
+export class HomeComponent {
+
+    title: string = "Angular web";
+    cls = "red green";
+    style = 'background-color:yellow'
+
+    getTitle(): string {
+        //update 
+        return this.title;
+    }
+
+    onClick(param) {
+        console.log("button clicked", param);
+    }
+
+    onChanged() {
+        console.log("Selection chagned");
+    }
+
+    //truthy 
+    //falsy  0, "", false, undefined, null, Nan
+
+
+}
