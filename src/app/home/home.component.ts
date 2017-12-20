3fd3268fbfcd58d292cca9ec46c66ba0b27f3400
Mujib517 <mujib517@gmail.com>
2017-12-20 08:59:00 +0530

    NgFor

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,51 +4,33 @@ import { Component } from "@angular/core";
 @Component({
     selector: 'app-home',
     template: ` 
-
-   
-
+    
     <table border="1">
         <tr>
-            <td>Value</td>
-            <td>Index</td>
-            <td>Even</td>
-            <td>Odd</td>
-            <td>First</td>
-            <td>Last</td>
+            <td>Id</td>
+            <td>Brand</td>
+            <td>Model</td>
+            <td>Price</td>
+            <td>InStock</td>
         </tr>
-        <tr *ngFor="let number of arr;let idx=index;let evn=even;let odd=odd;let fst=first;let lst=last;" [ngClass]="evn?'fg':'fg2'">
-            <td>{{number}}</td>
-            <td>{{idx}}</td>
-            <td>{{evn}}</td>
-            <td>{{odd}}</td>
-            <td>{{fst}}</td>
-            <td>{{lst}}</td>
+        <tr *ngFor="let product of products">
+            <td>{{product.id}}</td>
+            <td>{{product.brand}}</td>
+            <td>{{product.model}}</td>
+            <td>{{product.price}}</td>
+            <td>
+                <input type="checkbox" disabled="true" [checked]="product.inStock"/>
+            </td>
         </tr>
-        
     </table>
 
-
-    <h1 [ngClass]="obj">{{title}}</h1>
-    <h1 [ngStyle]="style">{{title}}</h1>
-
-    <input type="text" [(ngModel)]="title"/>
-
-    <div [ngSwitch]="priority">
-        <h3 *ngSwitchCase="'high'">High priority Task</h3>
-        <h3 *ngSwitchCase="'medium'">Medium priority Task</h3>
-        <h3 *ngSwitchCase="'low'">Low priority Task</h3>
-        <h3 *ngSwitchDefault>Very low priority</h3>
-    </div>
 
     `
 })
 export class HomeComponent {
 
-    arr: number[] = [1, 2, 3, 4, 5];
-    title: string = "Home Page";
-    style = { 'background-color': 'blue', 'border': '3px solid black' };
-
-    priority: string = "lo111w";
-    cls = ["red", "green"];
-    obj = { red: false, green: true };
+    products = [{ id: 1, brand: "Apple", model: "Iphone 5S", price: 200, inStock: false },
+    { id: 2, brand: "Apple", model: "Iphone 7S", price: 700, inStock: true },
+    { id: 3, brand: "Samsung", model: "Galaxy S8", price: 1200, inStock: true },
+    { id: 4, brand: "Samsung", model: "Galaxy S7", price: 800, inStock: true }];
 }
