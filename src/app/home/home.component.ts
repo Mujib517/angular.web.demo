ab13a5c1ceef8c7e8cfe796d4d3c8f7ab6c008b7
Mujib517 <mujib517@gmail.com>
2017-12-20 08:50:51 +0530

    Directives

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,43 +3,52 @@ import { Component } from "@angular/core";
 //ES6 Module
 @Component({
     selector: 'app-home',
-    template: `<h1 [class]="cls">{{getTitle()}}</h1>
-    
-    <h1 [style]="style">{{title?"Yes":"No"}}</h1>
+    template: ` 
 
-    <button [disabled]="getTitle()">Click me</button>
+   
 
-    <button (click)="onClick(1)">Button 2</button>
+    <table border="1">
+        <tr>
+            <td>Value</td>
+            <td>Index</td>
+            <td>Even</td>
+            <td>Odd</td>
+            <td>First</td>
+            <td>Last</td>
+        </tr>
+        <tr *ngFor="let number of arr;let idx=index;let evn=even;let odd=odd;let fst=first;let lst=last;" [ngClass]="evn?'fg':'fg2'">
+            <td>{{number}}</td>
+            <td>{{idx}}</td>
+            <td>{{evn}}</td>
+            <td>{{odd}}</td>
+            <td>{{fst}}</td>
+            <td>{{lst}}</td>
+        </tr>
+        
+    </table>
 
-    <select (change)="onChanged()">
-        <option>Apple</option>
-        <option>Samsung</option>
-        <option>Sony</option>
-    </select>
+
+    <h1 [ngClass]="obj">{{title}}</h1>
+    <h1 [ngStyle]="style">{{title}}</h1>
+
+    <input type="text" [(ngModel)]="title"/>
+
+    <div [ngSwitch]="priority">
+        <h3 *ngSwitchCase="'high'">High priority Task</h3>
+        <h3 *ngSwitchCase="'medium'">Medium priority Task</h3>
+        <h3 *ngSwitchCase="'low'">Low priority Task</h3>
+        <h3 *ngSwitchDefault>Very low priority</h3>
+    </div>
 
     `
 })
 export class HomeComponent {
 
-    title: string = "Angular web";
-    cls = "red green";
-    style = 'background-color:yellow'
+    arr: number[] = [1, 2, 3, 4, 5];
+    title: string = "Home Page";
+    style = { 'background-color': 'blue', 'border': '3px solid black' };
 
-    getTitle(): string {
-        //update 
-        return this.title;
-    }
-
-    onClick(param) {
-        console.log("button clicked", param);
-    }
-
-    onChanged() {
-        console.log("Selection chagned");
-    }
-
-    //truthy 
-    //falsy  0, "", false, undefined, null, Nan
-
-
+    priority: string = "lo111w";
+    cls = ["red", "green"];
+    obj = { red: false, green: true };
 }
