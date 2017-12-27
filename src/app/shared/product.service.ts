07b43c6d294aa7bf642df4c10d1a02122888de97
Mujib517 <mujib517@gmail.com>
2017-12-27 07:57:12 +0530

    Environment.ts

--- src/app/shared/product.service.ts
+++ src/app/shared/product.service.ts
@@ -1,17 +1,20 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
+import { environment } from "../../environments/environment";
 
 
 @Injectable()
 export class ProductService {
 
+    private _baseUrl = environment.baseUrl;
+
     constructor(private http: HttpClient) { }
 
     get() {
-        return this.http.get("https://express-api.herokuapp.com/api/products");
+        return this.http.get(`${this._baseUrl}api/products`);
     }
 
     save(product) {
-        return this.http.post("https://express-api.herokuapp.com/api/products", product);
+        return this.http.post(`${this._baseUrl}api/products`, product);
     }
 }
