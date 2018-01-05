e0807dd620b6a11f7b1cc75d7b0a9b46e303652c
Mujib517 <mujib517@gmail.com>
2018-01-05 07:55:15 +0530

    Change detection

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,17 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-about',
   template: `
     <h1>About Page</h1>
+
+    <h1>{{count}}</h1>
+
+    <button (click)="onClick()">++</button>
+
   `,
   styles: []
 })
 export class AboutComponent implements OnInit {
+  count: number = 0;
 
-  constructor() { }
+  //asynchronous operation
+  // web svc, events, setTimeout, setInterval
+
+  constructor(private http: HttpClient) { }
+
+  onClick() {
+    this.count++;
+  }
 
   ngOnInit() {
+    this.http.get("https://express-api.herokuapp.com/api/products")
+      .subscribe(
+      (res) => this.count = 100
+      )
   }
 
 }
